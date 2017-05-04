2fe26c6cec42c87f16f2951a70fa39787dfebb72
Alexandre ROCHARD <arochard@e1r9p16.42.fr>
2017-05-04 17:00:44 +0200

    ff

---
+++ SwiftyCompanion/SwiftyCompanion/ApiConnection.swift
@@ -0,0 +1,18 @@
+//
+//  ApiConnection.swift
+//  SwiftyCompanion
+//
+//  Created by Alexandre ROCHARD on 5/4/17.
+//  Copyright © 2017 Alexandre ROCHARD. All rights reserved.
+//
+
+import UIKit
+
+class ApiConnection: NSObject
+{
+    let UID = "36affbedb3b4ff95ce9d37389f81668805bff78d2074132bbd815e6d38a80085"
+    let secret = "d6036e8fd3c0041ddade2eea6e5c8525bed975d9704386cd477bee4d75776be7"
+    
+    
+    
+}
